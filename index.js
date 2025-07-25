9821ca13b99e51f8f9ef40b85e8e71c5cc01a2f7
yashgupta271 <yashgupta2711111@gmail.com>
2025-07-25 21:20:49 +0530

    upload

---
+++ index.js
@@ -0,0 +1,66 @@
+var win = 0;
+var lose = 0;
+var draw = 0;
+
+function bindClick() {
+  $(".items").off("click").on("click", function () {
+    // ✅ Move bot logic here
+    var randomNumber = Math.floor(Math.random() * 3);
+    var possibleOutputs = ["stone", "paper", "scissor"];
+    var botOutput = possibleOutputs[randomNumber];
+
+    var userInput = this.className.split(" ")[0];
+    $(".fix img").attr("src", "./images/stone.png");
+    $(".paper").html('<img src="./images/vs.png" alt="VS">');
+    $(".fix img").addClass("grow-shake");
+
+    // Disable further clicks
+    $(".items").off("click");
+
+    setTimeout(function () {
+      $(".fix img").removeClass("grow-shake");
+
+      $(".stone img").attr("src", "./images/" + userInput + ".png");
+      $(".scissor img").attr("src", "./images/" + botOutput + ".png");
+
+      $(".paper").html("<h2>" + whoWin(botOutput, userInput) + "</h2>");
+      $(".paper h2").addClass("h2j");
+      $(".win").text("win : " + win);
+      $(".lose").text("lose : " + lose);
+      $(".draw").text("draw : " + draw);
+
+      // ✅ Show restart button and bind handler once
+      $(".restart").show().off("click").on("click", function () {
+        $(".stone img").attr("src", "./images/stone.png");
+        $(".scissor img").attr("src", "./images/scissor.png");
+        $(".paper").html('');
+        $('<img>', {
+          src: './images/paper.png',
+          alt: 'Paper'
+        }).appendTo(".paper");
+
+        $(this).hide();
+        bindClick(); // Re-enable clicks
+      });
+    }, 2500);
+  });
+}
+
+bindClick(); // Initial bind
+
+function whoWin(bot, user) {
+  let result = "";
+  if (user == 'stone' && bot == "scissor" ||
+      user == 'paper' && bot == "stone" ||
+      user == 'scissor' && bot == "paper") {
+    result = "You Won!";
+    win += 1;
+  } else if (user == bot) {
+    result = "Draw!";
+    draw += 1;
+  } else {
+    result = "You Lose!";
+    lose += 1;
+  }
+  return result;
+}
